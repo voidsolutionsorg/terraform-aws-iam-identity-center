ffceb455b69a05c3f6231f3d3d71fa5ee46b072a
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2024-08-04 03:06:56 +0200

    fix: tf versions

--- versions.tf
+++ versions.tf
@@ -1,10 +1,9 @@
 terraform {
-  required_version = ">= 1.5"
-
+  required_version = ">= 1.5.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.65"
+      version = "~> 5.0"
     }
   }
 }
